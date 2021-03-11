bf490f5edab971cfd5a5bbaa85872ad06f3c4592
Krystian Osmelak <osmelakkrystian@gmail.com>
2021-03-11 17:52:24 +0100

    Dolne ikony

--- lib/gra.dart
+++ lib/gra.dart
@@ -4,12 +4,12 @@ class Gra extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var mediaQuery = MediaQuery.of(context).size;
-    Widget numberButton(String number) {
+    Widget numberButton(Widget number) {
       return Container(
         width: mediaQuery.width * 0.2,
         height: mediaQuery.height * 0.1,
         child: RaisedButton(
-          child: Text(number),
+          child: number,
           onPressed: () {},
           color: Colors.white,
         ),
@@ -24,21 +24,46 @@ class Gra extends StatelessWidget {
           ),
           Row(
             children: <Widget>[
-              numberButton('1'),
-              numberButton('2'),
-              numberButton('3'),
-              numberButton('4'),
-              numberButton('5'),
+              numberButton(Text('1')),
+              numberButton(Text('2')),
+              numberButton(Text('3')),
+              numberButton(Text('4')),
+              numberButton(Text('5')),
             ],
           ),
           Row(
             children: <Widget>[
-              numberButton('6'),
-              numberButton('7'),
-              numberButton('8'),
-              numberButton('9'),
-              numberButton('X'),
+              numberButton(Text('6')),
+              numberButton(Text('7')),
+              numberButton(Text('8')),
+              numberButton(Text('9')),
+              numberButton(Icon(Icons.delete)),
             ],
+          ),
+          SizedBox(
+            height: mediaQuery.height * 0.1,
+          ),
+          Container(
+            child: Row(
+              children: [
+                Icon(
+                  Icons.refresh,
+                  size: 60,
+                ),
+                Icon(
+                  Icons.undo,
+                  size: 60,
+                ),
+                Icon(
+                  Icons.done,
+                  size: 60,
+                ),
+                Icon(
+                  Icons.edit,
+                  size: 60,
+                ),
+              ],
+            ),
           ),
         ],
       ),
